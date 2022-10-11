394d1d0b0e411212a5e8b6b9066bb68562ddd38d
yaswu <putriyasminarahmaz@gmail.com>
2022-10-11 21:42:12 +0700

    second commit
    edit 3 files, delete 1 file

--- src/Navbar.js
+++ src/Navbar.js
@@ -3,7 +3,11 @@ const Navbar = () => {
         <h1>My _World</h1>
         <div className="links">
             <a href="/">Home </a>
-            <a href="/create">_World </a>
+            <a href="/create" style={{
+                color: "white",
+                backgroundColor: '#f1356d',
+                borderRadius: '8px'
+            }}>_World </a>
             <a href="/">Cheers </a>
             <a href="/">Fallin'Flower </a>
         </div>
