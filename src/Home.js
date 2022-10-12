6be753ba1f86c8ff3e1b84694e1eef83c0acf8bc
yaswu <putriyasminarahmaz@gmail.com>
2022-10-12 20:38:00 +0700

    third commit
    edit 2 files

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,23 @@
 const Home = () => {
+    
+    const handleClick = (e) => {
+        console.log('let\'s start your interview', e.target);
+    }
+
+    const handleClickAgain = (name, e) => {
+        console.log('hello ' + name, e.target);
+    }
+
+    const handleClickThis = (name, e) => {
+        console.log('I\'m ' + name, e.target);
+    }
+
     return ( 
         <div className="home">
             <h2>Homepage</h2>
+            <button onClick={(e) => handleClickAgain('minghao', e)}>Click me first</button>
+            <button onClick={(e) => handleClickThis('jeonghan', e)}>Then click me</button>
+            <button onClick={handleClick}>Last pls click Me</button>
         </div>
      );
 }
